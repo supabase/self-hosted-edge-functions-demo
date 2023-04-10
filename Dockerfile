9f8e7828be98ddf3de99afb9d23b5fccf5d8e9be
Lakshan Perera <lakshan@laktek.com>
2023-04-11 09:44:12 +1000

    fix: add support for JWT verification

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/supabase/edge-runtime:v1.2.4
+FROM ghcr.io/supabase/edge-runtime:v1.2.12
 
 COPY ./functions /home/deno/functions
 CMD [ "start", "--main-service", "/home/deno/functions/main" ]
--- functions/main/index.ts
+++ functions/main/index.ts
@@ -2,7 +2,54 @@ import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
 
 console.log('main function started');
 
+const JWT_SECRET = Deno.env.get("JWT_SECRET");
+const VERIFY_JWT = Deno.env.get("VERIFY_JWT") === "true";
+
+function getAuthToken(req: Request) {
+  const authHeader = req.headers.get("authorization");
+  if (!authHeader) {
+    throw new Error("Missing authorization header");
+  }
+  const [bearer, token] = authHeader.split(" ");
+  if (bearer !== "Bearer") {
+    throw new Error(`Auth header is not 'Bearer {token}'`);
+  }
+  return token;
+}
+
+async function verifyJWT(jwt: string): Promise<boolean> {
+  const encoder = new TextEncoder();
+  const secretKey = encoder.encode(JWT_SECRET);
+  try {
+    await jose.jwtVerify(jwt, secretKey);
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+  return true;
+}
+
 serve(async (req: Request) => {
+  if (req.method !== "OPTIONS" && VERIFY_JWT) {
+    try {
+      const token = getAuthToken(req);
+      const isValidJWT = await verifyJWT(token);
+
+      if (!isValidJWT) {
+        return new Response(
+          JSON.stringify({ msg: "Invalid JWT"}),
+          { status: 401, headers: { "Content-Type": "application/json" } },
+        ) 
+      }
+    } catch (e) {
+      console.error(e);
+      return new Response(
+        JSON.stringify({ msg: e.toString()}),
+        { status: 401, headers: { "Content-Type": "application/json" } },
+      )
+    }
+  }
+
   const url = new URL(req.url);
   const {pathname} = url;
   const path_parts = pathname.split("/");
@@ -20,7 +67,7 @@ serve(async (req: Request) => {
   console.error(`serving the request with ${service_path}`);
 
   const memory_limit_mb = 150;
-  const worker_timeout_ms = 5 * 60 * 1000;
+  const worker_timeout_ms = 1 * 60 * 1000;
   const no_module_cache = false;
   const import_map_path = null;
   const env_vars = [];
@@ -33,7 +80,7 @@ serve(async (req: Request) => {
       import_map_path,
       env_vars
     });
-    return worker.fetch(req);
+    return await worker.fetch(req);
   } catch (e) {
     const error = { msg: e.toString() }
     return new Response(
